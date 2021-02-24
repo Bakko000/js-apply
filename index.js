a6bd82775fe0fee5b3bcc3ed35a735c866e6fa37
Bakko000 <corrado.baccheschi@gmail.com>
2021-02-24 15:22:42 +0100

    2 files modified

--- index.js
+++ index.js
@@ -3,3 +3,29 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
+var Colours = ["rosso", "verde", "giallo", "blu", "arancio", "viola", "rosa", "marrone", "nero"];
+
+// Funzione
+ function myFunction() {
+
+    col = document.getElementById("myid").value;
+     marca = document.getElementById("myid2").value;
+      var Auto = { 
+        colore: col, 
+        marca: marca 
+        };
+         
+         col = col.toLowerCase();
+         marca = marca.toLowerCase();
+
+        if(Colours.indexOf(col) < 0){
+          document.getElementById("demo2").style.display = "block";
+          document.getElementById("demo").style.display = "none";
+
+        }
+         else {
+         document.getElementById("demo").style.display = "block";
+         document.getElementById("demo2").style.display = "none";
+         document.getElementById("demo").innerHTML="<b>Hai scelto un auto di colore " + Auto["colore"] + " e di marca " + Auto["marca"] + "</b>";
+         }
+        } 
